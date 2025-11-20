de3424ab76a7ebbc0df371ed4849ece12e3c7f9a
j-asmin-e <jazzava77@icloud.com>
2025-11-20 13:13:02 +0000

    Update CheckGuess.java

--- CheckGuess.java
+++ CheckGuess.java
@@ -1,1 +1,15 @@
-class CheckGuess
+public class CheckGuess{
+  private final String secretWord;
+  public CheckGuess(String secretWord){
+    this.secretWord = secretWord;
+  }
+  public LetterStatus[] check(String guess) {
+      LetterStatus[] result = new LetterStatus[5];
+    return result;
+  }}
+  public String [] LetterStatus(String guess){
+    String [] result= []
+    for (i=0; i<5; ++i){
+      String letterResult = String.charAt[i].LetterCheck();
+      result.add(letterResult);
+    }
